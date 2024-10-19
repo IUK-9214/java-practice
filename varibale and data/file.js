6c8c0bd076d1ef06c2fcd8a4706902fc8e6d9bf2
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-10-19 20:29:43 +0500

    java script prctie

---
+++ varibale and data/file.js
@@ -0,0 +1,14 @@
+console.log("hey i am a console");
+var a = 23;
+var b =234;
+var c="here i am string";
+console.log(a, b, c );
+console.log(typeof a, typeof b , typeof c );
+console.log(a+b+c);
+let age=5;
+if(age>18){
+    console.log("you can drive");
+}
+else{
+    console.log("you can not drive");
+}
